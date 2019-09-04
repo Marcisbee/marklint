bdb3d613a8a16a232340c8978a21a3e3be09b7fe
Marcisbee <marcisbergmanis@gmail.com>
2019-09-04 18:25:35 +0300

    Fix lint & types

--- src/build-ast.test.js
+++ src/build-ast.test.js
@@ -1,6 +1,7 @@
 const {
   build,
   Markup,
+  // eslint-disable-next-line no-unused-vars
   HTMLElement,
   HTMLText,
 } = require('./build-ast');
@@ -20,7 +21,6 @@ describe('buildAst', () => {
         end: 0,
         sourceType: 'HTML',
         start: 0,
-        type: 'Markup',
       }));
     });
 
@@ -33,45 +33,89 @@ describe('buildAst', () => {
           end: 0,
           raw: '',
           start: 0,
-          type: 'HTMLText',
           value: '',
         }),
       ]);
     });
 
-    test('should return correct `openingElement` start, end', () => {
-      const input = `asd <a href="#link">This is a link</a> dsa`;
-      const output = build(input);
+    describe('correct start and end values', () => {
+      test('should return correct starting `text` start, end', () => {
+        const input = `asd <a href="#link">This is a link</a> dsa`;
+        const output = build(input);
 
-      const {
-        start,
-        end,
-      } = output.children[1].openingElement;
-      expect(input.substring(start, end)).toEqual('<a href="#link">');
-    });
+        /** @type {Partial<HTMLText>} */
+        const element = output.children[0];
+        const {
+          start,
+          end,
+        } = element;
+        expect(input.substring(start, end)).toEqual('asd ');
+      });
 
-    test('should return correct `openingElement.name` start, end', () => {
-      const input = `asd <a href="#link">This is a link</a> dsa`;
-      const output = build(input);
+      test('should return correct ending `text` start, end', () => {
+        const input = `asd <a href="#link">This is a link</a> dsa`;
+        const output = build(input);
 
-      /** @type {HTMLElement} */
-      const element = output.children[1];
-      const {
-        start,
-        end,
-      } = element.openingElement.name;
-      expect(input.substring(start, end)).toEqual('a');
-    });
+        /** @type {Partial<HTMLText>} */
+        const element = output.children[2];
+        const {
+          start,
+          end,
+        } = element;
+        expect(input.substring(start, end)).toEqual(' dsa');
+      });
 
-    test('should return correct `closingElement` start, end values', () => {
-      const input = `asd <a href="#link">This is a link</a> dsa`;
-      const output = build(input);
+      test('should return correct `openingElement` start, end', () => {
+        const input = `asd <a href="#link">This is a link</a> dsa`;
+        const output = build(input);
 
-      const {
-        start,
-        end,
-      } = output.children[1].closingElement;
-      expect(input.substring(start, end)).toEqual('</a>');
+        /** @type {Partial<HTMLElement>} */
+        const element = output.children[1];
+        const {
+          start,
+          end,
+        } = element.openingElement;
+        expect(input.substring(start, end)).toEqual('<a href="#link">');
+      });
+
+      test('should return correct `openingElement.name` start, end', () => {
+        const input = `asd <a href="#link">This is a link</a> dsa`;
+        const output = build(input);
+
+        /** @type {Partial<HTMLElement>} */
+        const element = output.children[1];
+        const {
+          start,
+          end,
+        } = element.openingElement.name;
+        expect(input.substring(start, end)).toEqual('a');
+      });
+
+      test('should return correct `closingElement` start, end values', () => {
+        const input = `asd <a href="#link">This is a link</a> dsa`;
+        const output = build(input);
+
+        /** @type {Partial<HTMLElement>} */
+        const element = output.children[1];
+        const {
+          start,
+          end,
+        } = element.closingElement;
+        expect(input.substring(start, end)).toEqual('</a>');
+      });
+
+      test('should return correct `closingElement.name` start, end', () => {
+        const input = `asd <a href="#link">This is a link</a> dsa`;
+        const output = build(input);
+
+        /** @type {Partial<HTMLElement>} */
+        const element = output.children[1];
+        const {
+          start,
+          end,
+        } = element.closingElement.name;
+        expect(input.substring(start, end)).toEqual('a');
+      });
     });
   });
 });
